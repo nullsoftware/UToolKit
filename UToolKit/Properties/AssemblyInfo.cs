02f7c4ce1e537cebbf8cd65ac43c154cb179de09
nullsoftware <tumyrthebot@gmail.com>
2022-02-24 21:17:37 +0200

    Added xaml definition.

---
+++ UToolKit/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Windows.Markup;
+
+[assembly: XmlnsPrefix("https://github.com/nullsoftware/UToolKit", "converters")]
+[assembly: XmlnsDefinition("https://github.com/nullsoftware/UToolKit", "NullSoftware.ToolKit.Converters")]
