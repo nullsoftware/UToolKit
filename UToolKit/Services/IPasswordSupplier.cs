2b8b9dd7c3c97c4d92573074acee1f837f7e02de
nullsoftware <tumyrthebot@gmail.com>
2022-02-24 21:06:54 +0200

    Added more documentation.

--- UToolKit/Services/IPasswordSupplier.cs
+++ UToolKit/Services/IPasswordSupplier.cs
@@ -7,14 +7,29 @@ using System.Threading.Tasks;
 
 namespace NullSoftware.Services
 {
+    /// <summary>
+    /// Password supplier.
+    /// </summary>
     public interface IPasswordSupplier
     {
+        /// <summary>
+        /// Occurs when the password of the PasswordBox changes.
+        /// </summary>
         event EventHandler PasswordChanged;
 
+        /// <summary>
+        /// Gets or sets password from/to PasswordBox.
+        /// </summary>
         string Password { get; set; }
 
+        /// <summary>
+        /// Gets secure password from PasswordBox.
+        /// </summary>
         SecureString SecurePassword { get; }
 
+        /// <summary>
+        /// Clears password from PasswordBox.
+        /// </summary>
         void Clear();
     }
 }
