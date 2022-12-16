0311236cb7b900832fbe19fd1cd6a19a3aa30a21
nullsoftware <tumyrthebot@gmail.com>
2022-12-16 14:31:32 +0200

    Added more documentation.

--- UToolKit/RelayCommand.cs
+++ UToolKit/RelayCommand.cs
@@ -39,16 +39,42 @@ namespace NullSoftware
 
         #region Constructors
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="RelayCommand"/> class,
+        /// using specified action to invoke on execution
+        /// and a function to query for determining if the command can execute.
+        /// </summary>
+        /// <param name="execute">
+        /// The action to invoke when <see cref="ICommand.Execute"/> is called.
+        /// </param>
+        /// <param name="canExecute">
+        /// The function to invoke when <see cref="ICommand.CanExecute"/> is called.
+        /// </param>
+        /// <exception cref="NullReferenceException">
+        /// Throws when <paramref name="execute"/> or <paramref name="canExecute"/> is null.
+        /// </exception>
         public RelayCommand(Action execute, Func<bool> canExecute)
         {
             if (execute == null)
                 throw new NullReferenceException(nameof(execute));
+            if (canExecute == null)
+                throw new NullReferenceException(nameof(canExecute));
 
             _execute = execute;
             _canExecute = canExecute;
         }
 
-        public RelayCommand(Action execute) : this(execute, null)
+        /// <summary>
+        /// Initializes a new instance of <see cref="RelayCommand"/> class,
+        /// using specified action to invoke on execution.
+        /// </summary>
+        /// <param name="execute">
+        /// The action to invoke when <see cref="ICommand.Execute"/> is called.
+        /// </param>
+        /// <exception cref="NullReferenceException">
+        /// Throws when <paramref name="execute"/> is null.
+        /// </exception>
+        public RelayCommand(Action execute) : this(execute, new Func<bool>(() => true))
         {
 
         }
@@ -60,7 +86,7 @@ namespace NullSoftware
         /// <inheritdoc/>
         public bool CanExecute(object parameter)
         {
-            return _canExecute == null ? true : _canExecute();
+            return _canExecute();
         }
 
         /// <inheritdoc/>
@@ -114,6 +140,20 @@ namespace NullSoftware
 
         #region Constructors
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="RelayCommand{T}"/> class,
+        /// using specified action to invoke on execution with specified parameter type
+        /// and a function to query for determining if the command can execute.
+        /// </summary>
+        /// <param name="execute">
+        /// The action to invoke when <see cref="ICommand.Execute"/> is called.
+        /// </param>
+        /// <param name="canExecute">
+        /// The function to invoke when <see cref="ICommand.CanExecute"/> is called.
+        /// </param>
+        /// <exception cref="NullReferenceException">
+        /// Throws when <paramref name="execute"/> or <paramref name="canExecute"/> is null.
+        /// </exception>
         public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
         {
             if (execute == null)
@@ -123,7 +163,17 @@ namespace NullSoftware
             _canExecute = canExecute;
         }
 
-        public RelayCommand(Action<T> execute) : this(execute, null)
+        /// <summary>
+        /// Initializes a new instance of <see cref="RelayCommand{T}"/> class,
+        /// using specified action to invoke on execution with specified parameter type.
+        /// </summary>
+        /// <param name="execute">
+        /// The action to invoke when <see cref="ICommand.Execute"/> is called.
+        /// </param>
+        /// <exception cref="NullReferenceException">
+        /// Throws when <paramref name="execute"/> is null.
+        /// </exception>
+        public RelayCommand(Action<T> execute) : this(execute, new Func<T, bool>((_) => true))
         {
 
         }
@@ -135,7 +185,7 @@ namespace NullSoftware
         /// <inheritdoc/>
         public bool CanExecute(object parameter)
         {
-            return _canExecute == null ? true : _canExecute((T)parameter);
+            return _canExecute((T)parameter);
         }
 
         /// <inheritdoc/>
