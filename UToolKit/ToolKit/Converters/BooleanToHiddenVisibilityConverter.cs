304f156b0ea8b49b6578c874c0c14fe74109c351
nullsoftware <tumyrthebot@gmail.com>
2022-12-16 23:42:36 +0200

    Added more documentation. Reworked converters.

--- UToolKit/ToolKit/Converters/BooleanToHiddenVisibilityConverter.cs
+++ UToolKit/ToolKit/Converters/BooleanToHiddenVisibilityConverter.cs
@@ -8,16 +8,45 @@ using System.Windows.Data;
 
 namespace NullSoftware.ToolKit.Converters
 {
+    /// <summary>
+    /// Represents the converter that converts <see cref="Boolean"/> values 
+    /// to and from <see cref="Visibility"/> enumeration values.
+    /// </summary>
     [ValueConversion(typeof(bool), typeof(Visibility))]
     public class BooleanToHiddenVisibilityConverter : IValueConverter, IMultiValueConverter
     {
         #region IValueConverter
 
+        /// <summary>
+        /// Converts a <see cref="Boolean"/> value to a <see cref="Visibility"/> enumeration value.
+        /// </summary>
+        /// <param name="value">
+        /// The Boolean value to convert. 
+        /// This value can be a standard Boolean value 
+        /// or a nullable Boolean value.
+        /// </param>
+        /// <param name="targetType">This parameter is not used.</param>
+        /// <param name="parameter">This parameter is not used.</param>
+        /// <param name="culture">This parameter is not used.</param>
+        /// <returns>
+        /// <see cref="Visibility.Visible"/> if value is true;
+        /// otherwise, <see cref="Visibility.Hidden"/>.
+        /// </returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (bool)value ? Visibility.Visible : Visibility.Hidden;
+            return (bool?)value == true ? Visibility.Visible : Visibility.Hidden;
         }
 
+        /// <summary>
+        /// Converts a <see cref="Visibility"/> enumeration value to a <see cref="Boolean"/> value.
+        /// </summary>
+        /// <param name="value">A <see cref="Visibility"/> enumeration value.</param>
+        /// <param name="targetType">This parameter is not used.</param>
+        /// <param name="parameter">This parameter is not used.</param>
+        /// <param name="culture">This parameter is not used.</param>
+        /// <returns>
+        /// true if value is <see cref="Visibility.Visible"/>; otherwise, false.
+        /// </returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return (Visibility)value == Visibility.Visible;
@@ -27,11 +56,37 @@ namespace NullSoftware.ToolKit.Converters
 
         #region IMultiValueConverter
 
+        /// <summary>
+        /// Converts the array of <see cref="Boolean"/> values to a <see cref="Visibility"/> enumeration value.
+        /// </summary>
+        /// <param name="values">
+        /// The Boolean values to convert. 
+        /// This array item can be a standard Boolean value 
+        /// or a nullable Boolean value.
+        /// </param>
+        /// <param name="targetType">This parameter is not used.</param>
+        /// <param name="parameter">This parameter is not used.</param>
+        /// <param name="culture">This parameter is not used.</param>
+        /// <returns>
+        /// <see cref="Visibility.Visible"/> if all values is true;
+        /// otherwise, <see cref="Visibility.Hidden"/>.
+        /// </returns>
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            return values.All(t => (bool)t) ? Visibility.Visible : Visibility.Hidden;
+            return values.All(t => (bool?)t == true) ? Visibility.Visible : Visibility.Hidden;
         }
 
+        /// <summary>
+        /// Converts a <see cref="Visibility"/> enumeration value to a array of <see cref="Boolean"/> values.
+        /// </summary>
+        /// <param name="value">A <see cref="Visibility"/> enumeration value.</param>
+        /// <param name="targetTypes">This parameter is not used.</param>
+        /// <param name="parameter">This parameter is not used.</param>
+        /// <param name="culture">This parameter is not used.</param>
+        /// <returns>
+        /// bool array with positive values if value is <see cref="Visibility.Visible"/>;
+        /// otherwise, array with negative values.
+        /// </returns>
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             bool isVisible = (Visibility)value == Visibility.Visible;
--- UToolKit/ToolKit/Converters/InverseBooleanConverter.cs
+++ UToolKit/ToolKit/Converters/InverseBooleanConverter.cs
@@ -8,14 +8,42 @@ using System.Windows.Data;
 
 namespace NullSoftware.ToolKit.Converters
 {
+    /// <summary>
+    /// Represents the converter that converts <see cref="Boolean"/> values 
+    /// to inverse <see cref="Boolean"/> values.
+    /// </summary>
     [ValueConversion(typeof(bool), typeof(bool))]
     public class InverseBooleanConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a <see cref="Boolean"/> value to a inverse <see cref="Boolean"/> value.
+        /// </summary>
+        /// <param name="value">
+        /// The Boolean value to convert. 
+        /// </param>
+        /// <param name="targetType">This parameter is not used.</param>
+        /// <param name="parameter">This parameter is not used.</param>
+        /// <param name="culture">This parameter is not used.</param>
+        /// <returns>
+        /// Inverse <see cref="Boolean"/> value.
+        /// </returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return !((bool)value);
         }
 
+        /// <summary>
+        /// Converts a <see cref="Boolean"/> value to a inverse <see cref="Boolean"/> value.
+        /// </summary>
+        /// <param name="value">
+        /// The Boolean value to convert. 
+        /// </param>
+        /// <param name="targetType">This parameter is not used.</param>
+        /// <param name="parameter">This parameter is not used.</param>
+        /// <param name="culture">This parameter is not used.</param>
+        /// <returns>
+        /// Inverse <see cref="Boolean"/> value.
+        /// </returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return !((bool)value);
